a8db86afcef11a35dbf4ae6944186d72dd5dc77d
Vadim_S <v.senchenko@rostvert.ru>
2023-01-21 12:55:31 +0300

    Форма с формулами

--- src/formulaCalc.dpr
+++ src/formulaCalc.dpr
@@ -2,12 +2,14 @@ program formulaCalc;
 
 uses
   Forms,
-  CalculatorU in 'CalculatorU.pas' {CalculatorF};
+  CalculatorU in 'CalculatorU.pas' {CalculatorF},
+  HelpFormulasU in 'HelpFormulasU.pas' {HelpFormulasF};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TCalculatorF, CalculatorF);
+  Application.CreateForm(THelpFormulasF, HelpFormulasF);
   Application.Run;
 end.
